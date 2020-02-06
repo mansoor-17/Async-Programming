c522e9e14bca019f6d8f8d09cf2f2f35d30725bb
ip11 <iprash011@gmail.com>
2020-02-06 10:56:38 +0530

    1.js

--- 1.js
+++ 1.js
@@ -3,3 +3,20 @@
 //1) FCB (Function Call Backs)
 //2) Promises
 //3) Async Await
+
+function addNumber(a, b) {
+	console.log('In call stack of addNumber');
+	return a + b;
+}
+
+function multiplyNumber(a, b) {
+	console.log('In call stack of multiplyNumber');
+	var c = 0;
+	setTimeout(function() {
+		c = addNumber(a, b);
+		console.log('Return value from addNumber - ' + c);
+	}, 1000);
+	return c * b;
+}
+
+console.log('Final Output - ' + multiplyNumber(2, 3));
